05c3cfe49493aaa9dfc7873e521f63c335dd513a
233-Tencent4 <1369661643@qq.com>
2023-04-25 23:50:01 +0800

    🔥 暂时下掉steam订阅相关功能,太影响性能了

--- hoshino/modules/mhw/mhw.py
+++ hoshino/modules/mhw/mhw.py
@@ -268,97 +268,100 @@ async def delete_code():
 # 添加mh订阅 steamUID
 @sv.on_prefix("添加mh订阅")
 async def add_mh_subscribe(bot, ev: CQEvent):
-    # 获取订阅 UID
-    steam_uid = ev.message.extract_plain_text().strip()
-    # 获取群昵称
-    qqnickname = ev.sender["card"] or ev.sender["nickname"]
-    # 如果 steam_uid 不在steam_uid_game_json 中, 则说明当前不在订阅该用户 steam 状态， 此时其也必定不在 steam_uid_qqnickname_json 中
-    if steam_uid not in steam_uid_game_json:
-        response = httpx.get(PLAYER_SUMMARY.format(STEAM_APIKEY, steam_uid)).json()
-        # 获取 steam 昵称
-        steam_nickname = response["response"]["players"][0]["personaname"]
-        # 如果 steam_uid 也不在 steam_uid_nickname_json 中, 则说明从未订阅过该用户 steam 状态, 需要更新一下 steam_uid_nickname_json
-        if steam_uid not in steam_uid_nickname_json:
-            # 将 steam 昵称添加到 steam_uid_nickname_json 映射中
-            steam_uid_nickname_json[steam_uid] = steam_nickname
-            # 更新 json 文件
-            steam_uid_nickname_path.write_text(
-                json.dumps(steam_uid_nickname_json, indent=4)
-            )
-        # 将 steam_uid 和 qqnickname 添加到 steamuid_qqnickname_json 映射中
-        steamuid_qqnickname_json[steam_uid] = qqnickname
-        # 获取该用户正在玩的游戏的名称(如果没有在玩游戏则为 "没在玩游戏", 然后将其添加到 steam_uid_game_json 映射中)
-        if "gameid" in response["response"]["players"][0]:
-            steam_game = response["response"]["players"][0]["gameextrainfo"]
-            # 如果 steam_game 不在 MH_GAME_NAME_LIST 中, 则说明该用户不在玩 MH, 则 MH_game 状态为 None
-            if steam_game not in MH_GAME_NAME_LIST:
-                mh_game = None
-            else:
-                mh_game = steam_game
-                await bot.send(ev, f"{steam_nickname}, 正在玩: {steam_game}")
-        else:
-            steam_game = None
-            mh_game = None
-        steam_uid_game_json[steam_uid] = steam_game
-        steam_uid_mh_game_json[steam_uid] = mh_game
-        # 更新 json 文件
-        steamuid_qqnickname_json_path.write_text(
-            json.dumps(steamuid_qqnickname_json, indent=4)
-        )
-        steam_uid_game_path.write_text(json.dumps(steam_uid_game_json, indent=4))
-        steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
-        await bot.send(ev, f"> {qqnickname} \n 怪猎动态订阅成功, steam 昵称为 {steam_nickname}")
-    # 如果 steam_uid 在 steam_uid_game_json 中, 但是不在 steamuid_qqnickname_json 中, 则说明当前订阅了该用户的steam状态但是没有订阅怪猎状态
-    elif steam_uid not in steamuid_qqnickname_json:
-        steamuid_qqnickname_json[steam_uid] = qqnickname
-        steamuid_qqnickname_json_path.write_text(
-            json.dumps(steamuid_qqnickname_json, indent=4)
-        )
-        steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
-        steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
-        await bot.send(
-            ev,
-            f"> {qqnickname} \n steam动态已订阅, 添加怪猎动态订阅成功, steam 昵称为 {steam_uid_nickname_json[steam_uid]})",
-        )
-        return
-    elif qqnickname == steamuid_qqnickname_json[steam_uid]:
-        await bot.send(ev, f"已经订阅过该群友, steam 昵称为 {steam_uid_nickname_json[steam_uid]}")
-    else:
-        steamuid_qqnickname_json[steam_uid] = qqnickname
-        steamuid_qqnickname_json_path.write_text(
-            json.dumps(steamuid_qqnickname_json, indent=4)
-        )
-        await bot.send(
-            ev,
-            f"> {qqnickname} \n 修改订阅群名片成功, 已更正为 {steamuid_qqnickname_json[steam_uid]})",
-        )
+    await bot.send(ev, "steam订阅功能维护中......")
+    # # 获取订阅 UID
+    # steam_uid = ev.message.extract_plain_text().strip()
+    # # 获取群昵称
+    # qqnickname = ev.sender["card"] or ev.sender["nickname"]
+    # # 如果 steam_uid 不在steam_uid_game_json 中, 则说明当前不在订阅该用户 steam 状态， 此时其也必定不在 steam_uid_qqnickname_json 中
+    # if steam_uid not in steam_uid_game_json:
+    #     response = httpx.get(PLAYER_SUMMARY.format(STEAM_APIKEY, steam_uid)).json()
+    #     # 获取 steam 昵称
+    #     steam_nickname = response["response"]["players"][0]["personaname"]
+    #     # 如果 steam_uid 也不在 steam_uid_nickname_json 中, 则说明从未订阅过该用户 steam 状态, 需要更新一下 steam_uid_nickname_json
+    #     if steam_uid not in steam_uid_nickname_json:
+    #         # 将 steam 昵称添加到 steam_uid_nickname_json 映射中
+    #         steam_uid_nickname_json[steam_uid] = steam_nickname
+    #         # 更新 json 文件
+    #         steam_uid_nickname_path.write_text(
+    #             json.dumps(steam_uid_nickname_json, indent=4)
+    #         )
+    #     # 将 steam_uid 和 qqnickname 添加到 steamuid_qqnickname_json 映射中
+    #     steamuid_qqnickname_json[steam_uid] = qqnickname
+    #     # 获取该用户正在玩的游戏的名称(如果没有在玩游戏则为 "没在玩游戏", 然后将其添加到 steam_uid_game_json 映射中)
+    #     if "gameid" in response["response"]["players"][0]:
+    #         steam_game = response["response"]["players"][0]["gameextrainfo"]
+    #         # 如果 steam_game 不在 MH_GAME_NAME_LIST 中, 则说明该用户不在玩 MH, 则 MH_game 状态为 None
+    #         if steam_game not in MH_GAME_NAME_LIST:
+    #             mh_game = None
+    #         else:
+    #             mh_game = steam_game
+    #             await bot.send(ev, f"{steam_nickname}, 正在玩: {steam_game}")
+    #     else:
+    #         steam_game = None
+    #         mh_game = None
+    #     steam_uid_game_json[steam_uid] = steam_game
+    #     steam_uid_mh_game_json[steam_uid] = mh_game
+    #     # 更新 json 文件
+    #     steamuid_qqnickname_json_path.write_text(
+    #         json.dumps(steamuid_qqnickname_json, indent=4)
+    #     )
+    #     steam_uid_game_path.write_text(json.dumps(steam_uid_game_json, indent=4))
+    #     steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
+    #     await bot.send(ev, f"> {qqnickname} \n 怪猎动态订阅成功, steam 昵称为 {steam_nickname}")
+    # # 如果 steam_uid 在 steam_uid_game_json 中, 但是不在 steamuid_qqnickname_json 中, 则说明当前订阅了该用户的steam状态但是没有订阅怪猎状态
+    # elif steam_uid not in steamuid_qqnickname_json:
+    #     steamuid_qqnickname_json[steam_uid] = qqnickname
+    #     steamuid_qqnickname_json_path.write_text(
+    #         json.dumps(steamuid_qqnickname_json, indent=4)
+    #     )
+    #     steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
+    #     steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
+    #     await bot.send(
+    #         ev,
+    #         f"> {qqnickname} \n steam动态已订阅, 添加怪猎动态订阅成功, steam 昵称为 {steam_uid_nickname_json[steam_uid]})",
+    #     )
+    #     return
+    # elif qqnickname == steamuid_qqnickname_json[steam_uid]:
+    #     await bot.send(ev, f"已经订阅过该群友, steam 昵称为 {steam_uid_nickname_json[steam_uid]}")
+    # else:
+    #     steamuid_qqnickname_json[steam_uid] = qqnickname
+    #     steamuid_qqnickname_json_path.write_text(
+    #         json.dumps(steamuid_qqnickname_json, indent=4)
+    #     )
+    #     await bot.send(
+    #         ev,
+    #         f"> {qqnickname} \n 修改订阅群名片成功, 已更正为 {steamuid_qqnickname_json[steam_uid]})",
+    #     )
 
-    return
+    # return
 
 
 # 删除mh订阅 steamUID
 @sv.on_prefix("删除mh订阅")
 async def delete_mh_subscribe(bot, ev: CQEvent):
-    # 获取订阅 UID
-    steam_uid = ev.message.extract_plain_text().strip()
-    # 获取群昵称
-    nickname = ev.sender["card"] or ev.sender["nickname"]
-    # 如果没有订阅过该 UID, 则返回
-    if steam_uid not in steamuid_qqnickname_json:
-        await bot.send(ev, "没有订阅过该群友怪猎动态")
-    else:
-        del steamuid_qqnickname_json[steam_uid]
-        steamuid_qqnickname_json_path.write_text(
-            json.dumps(steamuid_qqnickname_json, indent=4)
-        )
-        # 同时取消掉 steam_uid_game_json 中的订阅
-        del steam_uid_game_json[steam_uid]
-        steam_uid_game_path.write_text(json.dumps(steam_uid_game_json, indent=4))
-        # 同时取消掉 steam_uid_mh_game_json 中的订阅
-        del steam_uid_mh_game_json[steam_uid]
-        steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
-        await bot.send(ev, f"> {nickname} \n 怪猎动态取消订阅成功")
-    return
+    # # 获取订阅 UID
+    # steam_uid = ev.message.extract_plain_text().strip()
+    # # 获取群昵称
+    # nickname = ev.sender["card"] or ev.sender["nickname"]
+    # # 如果没有订阅过该 UID, 则返回
+    # if steam_uid not in steamuid_qqnickname_json:
+    #     await bot.send(ev, "没有订阅过该群友怪猎动态")
+    # else:
+    #     del steamuid_qqnickname_json[steam_uid]
+    #     steamuid_qqnickname_json_path.write_text(
+    #         json.dumps(steamuid_qqnickname_json, indent=4)
+    #     )
+    #     # 同时取消掉 steam_uid_game_json 中的订阅
+    #     del steam_uid_game_json[steam_uid]
+    #     steam_uid_game_path.write_text(json.dumps(steam_uid_game_json, indent=4))
+    #     # 同时取消掉 steam_uid_mh_game_json 中的订阅
+    #     del steam_uid_mh_game_json[steam_uid]
+    #     steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
+    #     await bot.send(ev, f"> {nickname} \n 怪猎动态取消订阅成功")
+    # return
+    await bot.send(ev, "steam订阅功能维护中......")
+
 
 
 # Monster Hunter: World
@@ -366,112 +369,117 @@ async def delete_mh_subscribe(bot, ev: CQEvent):
 
 
 # 没半分钟执行一次查询, 查询 steam_uid_game_json 中的用户游戏状态
-@sv.scheduled_job("cron", second="*/30")
-async def mh_subscribe():
-    print("开始查询怪猎动态")
-    # 重新读取 steam_uid_game_json
-    steam_uid_game_json = json.loads(steam_uid_game_path.read_text())
-    # 比较 steam_uid_game_json 和 steam_uid_mh_game_json 中的用户游戏状态
-    for steam_uid in steam_uid_mh_game_json:
-        # 获取群昵称
-        qqnickname = steamuid_qqnickname_json[steam_uid]
-        # 获取 steam 昵称
-        steam_nickname = steam_uid_nickname_json[steam_uid]
-        if steam_uid_mh_game_json[steam_uid] == steam_uid_game_json[steam_uid]:
-            continue
-        # 如果 steam_uid_mh_game_json 中的用户游戏状态为 None
-        if steam_uid_mh_game_json[steam_uid] is None:
-            # 如果 steam_uid_game_json 中的用户游戏状态在 MH_GAME_NAME_LIST 中
-            if steam_uid_game_json[steam_uid] in MH_GAME_NAME_LIST:
-                # 更新 steam_uid_mh_game_json 中的用户游戏状态
-                steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
-                # 推送 qqnickname - steam nickname 正在玩 steam_uid_game_json[steam_uid]
-                msg = f"{qqnickname}-{steam_nickname}, 正在玩: {steam_uid_game_json[steam_uid]}"
-                for gid in GROUP_LIST:
-                    await sv.bot.send_group_msg(group_id=int(gid), message=msg)
-        elif steam_uid_game_json[steam_uid] in MH_GAME_NAME_LIST:
-            # 推送 qqnickname - steam nickname 正在玩 steam_uid_game_json[steam_uid]
-            msg = f"{qqnickname}-{steam_nickname}, 不玩 {steam_uid_mh_game_json[steam_uid]} 了, 目前正在玩: {steam_uid_game_json[steam_uid]}"
-            for gid in GROUP_LIST:
-                await sv.bot.send_group_msg(group_id=int(gid), message=msg)
-            # 更新 steam_uid_mh_game_json 中的用户游戏状态
-            steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
-        else:
-            # 推送 qqnickname - steam nickname 不在玩怪猎
-            msg = f"{qqnickname}-{steam_nickname}, 不玩 {steam_uid_mh_game_json[steam_uid]} 了"
-            for gid in GROUP_LIST:
-                await sv.bot.send_group_msg(group_id=int(gid), message=msg)
-            # 更新 steam_uid_mh_game_json 中的用户游戏状态
-            steam_uid_mh_game_json[steam_uid] = None
-    # 更新 steam_uid_mh_game_json
-    steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
-    print("怪猎动态查询结束")
+# @sv.scheduled_job("cron", second="*/30")
+# async def mh_subscribe():
+#     print("开始查询怪猎动态")
+#     # 重新读取 steam_uid_game_json
+#     steam_uid_game_json = json.loads(steam_uid_game_path.read_text())
+#     # 比较 steam_uid_game_json 和 steam_uid_mh_game_json 中的用户游戏状态
+#     for steam_uid in steam_uid_mh_game_json:
+#         # 获取群昵称
+#         qqnickname = steamuid_qqnickname_json[steam_uid]
+#         # 获取 steam 昵称
+#         steam_nickname = steam_uid_nickname_json[steam_uid]
+#         if steam_uid_mh_game_json[steam_uid] == steam_uid_game_json[steam_uid]:
+#             continue
+#         # 如果 steam_uid_mh_game_json 中的用户游戏状态为 None
+#         if steam_uid_mh_game_json[steam_uid] is None:
+#             # 如果 steam_uid_game_json 中的用户游戏状态在 MH_GAME_NAME_LIST 中
+#             if steam_uid_game_json[steam_uid] in MH_GAME_NAME_LIST:
+#                 # 更新 steam_uid_mh_game_json 中的用户游戏状态
+#                 steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
+#                 # 推送 qqnickname - steam nickname 正在玩 steam_uid_game_json[steam_uid]
+#                 msg = f"{qqnickname}-{steam_nickname}, 正在玩: {steam_uid_game_json[steam_uid]}"
+#                 for gid in GROUP_LIST:
+#                     await sv.bot.send_group_msg(group_id=int(gid), message=msg)
+#         elif steam_uid_game_json[steam_uid] in MH_GAME_NAME_LIST:
+#             # 推送 qqnickname - steam nickname 正在玩 steam_uid_game_json[steam_uid]
+#             msg = f"{qqnickname}-{steam_nickname}, 不玩 {steam_uid_mh_game_json[steam_uid]} 了, 目前正在玩: {steam_uid_game_json[steam_uid]}"
+#             for gid in GROUP_LIST:
+#                 await sv.bot.send_group_msg(group_id=int(gid), message=msg)
+#             # 更新 steam_uid_mh_game_json 中的用户游戏状态
+#             steam_uid_mh_game_json[steam_uid] = steam_uid_game_json[steam_uid]
+#         else:
+#             # 推送 qqnickname - steam nickname 不在玩怪猎
+#             msg = f"{qqnickname}-{steam_nickname}, 不玩 {steam_uid_mh_game_json[steam_uid]} 了"
+#             for gid in GROUP_LIST:
+#                 await sv.bot.send_group_msg(group_id=int(gid), message=msg)
+#             # 更新 steam_uid_mh_game_json 中的用户游戏状态
+#             steam_uid_mh_game_json[steam_uid] = None
+#     # 更新 steam_uid_mh_game_json
+#     steam_uid_mh_game_path.write_text(json.dumps(steam_uid_mh_game_json, indent=4))
+#     print("怪猎动态查询结束")
 
 
 # 谁在打猎?
 @sv.on_fullmatch("谁在打猎")
 async def who_is_playing_mh(bot, ev: CQEvent):
-    # 读取 steam_uid_mh_game_json
-    steam_uid_mh_game_json = json.loads(steam_uid_mh_game_path.read_text())
-    # 匹配非 null 的 UID
-    playing_mh_uid_list = [
-        steam_uid
-        for steam_uid in steam_uid_mh_game_json
-        if steam_uid_mh_game_json[steam_uid] is not None
-    ]
-    # 如果没有人在打猎
-    if not playing_mh_uid_list:
-        await bot.send(ev, "当前没有群友在打猎")
-        return
-    # 如果有人在打猎
-    msg = "====Monster Hunter====\n"
-    for steam_uid in playing_mh_uid_list:
-        # 获取群昵称
-        qqnickname = steamuid_qqnickname_json[steam_uid]
-        # 获取 steam 昵称
-        steam_nickname = steam_uid_nickname_json[steam_uid]
-        # 获取游戏名称
-        game_name = steam_uid_mh_game_json[steam_uid]
-        msg += f"{qqnickname}-{steam_nickname} 正在玩 {game_name}\n"
-    await bot.send(ev, msg)
+    # # 读取 steam_uid_mh_game_json
+    # steam_uid_mh_game_json = json.loads(steam_uid_mh_game_path.read_text())
+    # # 匹配非 null 的 UID
+    # playing_mh_uid_list = [
+    #     steam_uid
+    #     for steam_uid in steam_uid_mh_game_json
+    #     if steam_uid_mh_game_json[steam_uid] is not None
+    # ]
+    # # 如果没有人在打猎
+    # if not playing_mh_uid_list:
+    #     await bot.send(ev, "当前没有群友在打猎")
+    #     return
+    # # 如果有人在打猎
+    # msg = "====Monster Hunter====\n"
+    # for steam_uid in playing_mh_uid_list:
+    #     # 获取群昵称
+    #     qqnickname = steamuid_qqnickname_json[steam_uid]
+    #     # 获取 steam 昵称
+    #     steam_nickname = steam_uid_nickname_json[steam_uid]
+    #     # 获取游戏名称
+    #     game_name = steam_uid_mh_game_json[steam_uid]
+    #     msg += f"{qqnickname}-{steam_nickname} 正在玩 {game_name}\n"
+    # await bot.send(ev, msg)
+    await bot.send(ev, "steam订阅功能维护中......")
 
 
 # mh订阅列表
 @sv.on_rex(r"^mh订阅列表$")
 async def mh_subscribe_list(bot, ev: CQEvent):
-    # 读取 steam_uid_mh_game_json
-    steam_uid_mh_game_json = json.loads(steam_uid_mh_game_path.read_text())
-    msg = "====Monster Hunter====\n"
-    for steam_uid in steam_uid_mh_game_json:
-        # 获取群昵称
-        qqnickname = steamuid_qqnickname_json[steam_uid]
-        # 获取 steam 昵称
-        steam_nickname = steam_uid_nickname_json[steam_uid]
-        # 获取游戏名称
-        game_name = steam_uid_mh_game_json[steam_uid]
-        if game_name is None:
-            msg += f"{qqnickname}-{steam_nickname} 当前不在打猎\n"
-        else:
-            msg += f"{qqnickname}-{steam_nickname} 正在玩 {game_name}\n"
-    await bot.send(ev, msg)
+    # # 读取 steam_uid_mh_game_json
+    # steam_uid_mh_game_json = json.loads(steam_uid_mh_game_path.read_text())
+    # msg = "====Monster Hunter====\n"
+    # for steam_uid in steam_uid_mh_game_json:
+    #     # 获取群昵称
+    #     qqnickname = steamuid_qqnickname_json[steam_uid]
+    #     # 获取 steam 昵称
+    #     steam_nickname = steam_uid_nickname_json[steam_uid]
+    #     # 获取游戏名称
+    #     game_name = steam_uid_mh_game_json[steam_uid]
+    #     if game_name is None:
+    #         msg += f"{qqnickname}-{steam_nickname} 当前不在打猎\n"
+    #     else:
+    #         msg += f"{qqnickname}-{steam_nickname} 正在玩 {game_name}\n"
+    # await bot.send(ev, msg)
+    await bot.send(ev, "steam订阅功能维护中......")
+
 
 
 # 帮助mh订阅
 @sv.on_fullmatch("怎么订阅mh")
 async def mh_subscribe_help(bot, ev: CQEvent):
-    await bot.send(
-        ev,
-        '请在群内发送 "添加mh订阅 UID" 以添加订阅 \n\
-例如: "添加mh订阅 76561198846049157" \n\
-此处的UID为steam的UID, 在网页上登录Steam, 点击右上角的头像, 在网址栏中可以看到你的 Steam UID \n\
-也即是 "https://steamcommunity.com/profiles/76561198846049157" 中的 "76561198846049157" \n\
-有些用户的 Steam UID 为 "https://steamcommunity.com/id/xxx" 的形式, \n\
-此时可以点击右上角头像旁边的昵称下拉菜单 -> 账户明细 \n\
-然后就可以在 XXX 的账户字样下看到你的 Steam UID 了\n\
-手机端steam同样可以在账户明细页面查看到steam UID\n\
-不管是电脑端还是手机端都可以用UU免费加速\n\
-其他说明请发送 "帮助mhw" 来获取帮助',
-    )
+#     await bot.send(
+#         ev,
+#         '请在群内发送 "添加mh订阅 UID" 以添加订阅 \n\
+# 例如: "添加mh订阅 76561198846049157" \n\
+# 此处的UID为steam的UID, 在网页上登录Steam, 点击右上角的头像, 在网址栏中可以看到你的 Steam UID \n\
+# 也即是 "https://steamcommunity.com/profiles/76561198846049157" 中的 "76561198846049157" \n\
+# 有些用户的 Steam UID 为 "https://steamcommunity.com/id/xxx" 的形式, \n\
+# 此时可以点击右上角头像旁边的昵称下拉菜单 -> 账户明细 \n\
+# 然后就可以在 XXX 的账户字样下看到你的 Steam UID 了\n\
+# 手机端steam同样可以在账户明细页面查看到steam UID\n\
+# 不管是电脑端还是手机端都可以用UU免费加速\n\
+# 其他说明请发送 "帮助mhw" 来获取帮助',
+#     )
+    await bot.send(ev, "steam订阅功能维护中......")
+
 
 
 # mhw 帮助
@@ -483,10 +491,10 @@ async def mhw_help(bot, ev: CQEvent):
     msg += "删除我添加的集会码\n"
     msg += "昨日集会码\n"
     msg += "修改集会备注 集会码 备注\n"
-    msg += "添加mh订阅 steamUID\n"
-    msg += "删除mh订阅 steamUID\n"
-    msg += "怎么订阅mh\n"
-    msg += "mh订阅列表\n"
-    msg += "谁在打猎\n"
-    msg += "mh订阅(怪猎[世界,崛起]在线状态)会同步订阅者的qq群名片, 因此最好亲自订阅, 否则会串群名片, 或者在其他群友帮忙订阅后再自己重复订阅一遍以更新群名片\n"
+    # msg += "添加mh订阅 steamUID\n"
+    # msg += "删除mh订阅 steamUID\n"
+    # msg += "怎么订阅mh\n"
+    # msg += "mh订阅列表\n"
+    # msg += "谁在打猎\n"
+    # msg += "mh订阅(怪猎[世界,崛起]在线状态)会同步订阅者的qq群名片, 因此最好亲自订阅, 否则会串群名片, 或者在其他群友帮忙订阅后再自己重复订阅一遍以更新群名片\n"
     await bot.send(ev, msg)
